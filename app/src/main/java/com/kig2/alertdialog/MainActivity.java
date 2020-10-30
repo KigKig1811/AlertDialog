5c90977ebdfa01c7f8ff43717fe998b2a1ecfcad
Xoong1811 <ngockinh1811@gmail.com>
2020-10-30 20:47:59 +0700

    add

---
+++ app/src/main/java/com/kig2/alertdialog/MainActivity.java
@@ -0,0 +1,63 @@
+package com.kig2.alertdialog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.nio.IntBuffer;
+
+public class MainActivity extends AppCompatActivity {
+    Button mBtnAlertDialog;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mBtnAlertDialog=findViewById(R.id.btnAlert);
+
+        mBtnAlertDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context;
+                AlertDialog.Builder builder= new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Bạn có thông báo mới!!!");
+                builder.setMessage("Phiên bản đã có bản cập nhật mới!!!");
+                builder.setIcon(R.mipmap.ic_launcher);
+                builder.setCancelable(false);
+                // Singe Choice , multiple Choice
+                builder.setPositiveButton("Ok!", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();//tắt dialog
+                    }
+                });
+                builder.setNegativeButton("Cancel!", new DialogInterface.OnClickListener() {//Nút tiêu cực
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish(); //kill 1 activity
+                        //task Lần xử lý
+                        //stack lưu trữ lại ngăn xếp
+
+                    }
+                });
+                builder.setNeutralButton("Exit", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this,"Exit",Toast.LENGTH_LONG).show();
+                    }
+                });
+                builder.show();
+
+
+            }
+        });
+    }
+}
