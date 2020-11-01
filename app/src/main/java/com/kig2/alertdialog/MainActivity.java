7b60886914f470d135919ad5b92e099f9ae3e0bb
Xoong1811 <ngockinh1811@gmail.com>
2020-11-01 14:45:25 +0700

    mutiple CHoice

--- app/src/main/java/com/kig2/alertdialog/MainActivity.java
+++ app/src/main/java/com/kig2/alertdialog/MainActivity.java
@@ -14,6 +14,8 @@ import java.nio.IntBuffer;
 
 public class MainActivity extends AppCompatActivity {
     Button mBtnAlertDialog;
+    int mPosition;
+
 
 
     @Override
@@ -21,37 +23,51 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mPosition=-1;
+
         mBtnAlertDialog=findViewById(R.id.btnAlert);
 
         mBtnAlertDialog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Context context;
-                AlertDialog.Builder builder= new AlertDialog.Builder(MainActivity.this);
+                final AlertDialog.Builder builder= new AlertDialog.Builder(MainActivity.this);
                 builder.setTitle("Bạn có thông báo mới!!!");
               //  builder.setMessage("Phiên bản đã có bản cập nhật mới!!!");
                 builder.setIcon(R.mipmap.ic_launcher);
                 builder.setCancelable(false);
                 // Singe Choice , multiple Choice
                 final String[]  arrAnimals = {"Mèo","Chó","Voi","Gà","Khỉ","Heo"};
-                builder.setSingleChoiceItems(arrAnimals, -1, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int position) {
-                        Toast.makeText(getApplicationContext(), arrAnimals[position], Toast.LENGTH_SHORT).show();
-                    }
-                });
+                final boolean[] arrSelected = {false,false,false,false,false,false};
+               builder.setMultiChoiceItems(arrAnimals, arrSelected, new DialogInterface.OnMultiChoiceClickListener() {
+                   @Override
+                   public void onClick(DialogInterface dialog, int position, boolean isChecked) {
+
+                   }
+               });
                 builder.setPositiveButton("Ok!", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();//tắt dialog
+                        String result = "";
+                        for(int i=0;i < arrSelected.length;i++){
+                            if(arrSelected[i]){
+                                result += arrAnimals[i] +",";
+                            }
+                        }
+                        result=result.substring(0,result.length() - 3);
+                        Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
+
                     }
+
                 });
                 builder.setNegativeButton("Cancel!", new DialogInterface.OnClickListener() {//Nút tiêu cực
                     @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        finish(); //kill 1 activity
+                    public void onClick(DialogInterface dialog, int position) {
+                       finish(); //kill 1 activity
                         //task Lần xử lý
                         //stack lưu trữ lại ngăn xếp
+
+
 
                     }
                 });
